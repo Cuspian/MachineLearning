4710aaf0a44f99cb9f20eceb913ecba32e9b81a0
Cuspian <dk2926@nyu.edu>
2017-03-28 21:24:17 -0400

    Durations categorized

--- chordextractor.py
+++ chordextractor.py
@@ -31,6 +31,7 @@ class ChordExtractor(object):
         self.pitches = None
         self.normalizedPitches = []
         self.chordDurations = []
+        self.categorizedDurations = []
         self.chordGraph = chordgraph.ChordGraph()
     
     '''This method will take only those measures that are chord objects, and ignore the rest'''
@@ -75,7 +76,7 @@ class ChordExtractor(object):
                 
         return self.normalizedPitches
     
-    
+    '''A static method that converts a Duration object to a number'''
     @staticmethod
     def convertDurationToFloat(duration):
         durationAsString = str(duration)
@@ -85,7 +86,8 @@ class ChordExtractor(object):
         #if the duration is a fraction, convert it to a decimal
         
         if "/" in durationAsString:
-            durationAsFloat = float(durationAsString[0]) / float(durationAsString[2])
+            split = durationAsString.split("/")
+            durationAsFloat = float(split[0]) / float(split[1])
         #otherwise, keep it the same
         else:
             durationAsFloat = float(durationAsString)
@@ -93,24 +95,59 @@ class ChordExtractor(object):
         #keep only 2 decimal places.
         durationAsFloat = round(durationAsFloat, 2)
         return durationAsFloat
-        
+     
+    '''A method that collects all the chord durations and puts it into a vector'''
     def getChordDurations(self):
-        if self.chords:
-            return
-        self.extractChordObjects()
+        if not self.chords:
+            self.extractChordObjects()
         
         for chord in self.chords:
             if isinstance(chord, music21.chord.Chord):
                 duration = chord.duration #chord.duration is an object that 
-                                          #represents how many quarter notes a chord lasts
-                
+                                          #represents how long a chord is
                 #the problem is that duration objects that aren't multiples of 4 display as Fraction objects.
                 #to avoid this problem, parse the string representation of the object before creating vector.
                 durationNumber = ChordExtractor.convertDurationToFloat(duration)
-                self.chordDurations.append(durationNumber) #quarterLength is the number itself
+                self.chordDurations.append(durationNumber) 
         return self.chordDurations
     
-   
+    '''A method that will create a vector of ennumerated chord durations.  This is necessary to enable easy
+        counting of different chord length types. It goes without saying that creating a dict keyed on floats
+        is a very bad idea.'''
+    def getCategorizedDurations(self):
+        if not self.chordDurations:
+            self.getChordDurations()
+        
+        for duration in self.chordDurations:
+            category = ChordExtractor.categorize(duration)
+            self.categorizedDurations.append(category)
+            
+        return self.categorizedDurations
+    
+    '''Converts a duration number into a category for easy enumeration.'''
+    @staticmethod
+    def categorize(duration):
+        if duration<=0.1:
+            return 0
+        if duration<=0.2:
+            return 1
+        if duration<=0.3:
+            return 2
+        if duration<=0.4:
+            return 3
+        if duration<=0.5:
+            return 4
+        if duration<=0.6:
+            return 5
+        if duration<=0.7:
+            return 6
+        if duration<=0.8:
+            return 7
+        if duration<=0.9:
+            return 8
+        return 9
+    
+    
         
         
     ##TODO: figure out where the key signature is in the midi file parsed. 
